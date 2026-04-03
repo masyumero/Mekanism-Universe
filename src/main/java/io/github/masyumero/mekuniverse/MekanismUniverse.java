54ce440030983d2cfe563577a75eb97830b06098
masyumero <167562220+masyumero@users.noreply.github.com>
2026-04-03 16:06:18 +0900

    Rename mod to Mekanism:Universe and update dependencies in build.gradle and forge.versions.toml

--- src/main/java/io/github/masyumero/mekuniverse/MekanismUniverse.java
+++ src/main/java/io/github/masyumero/mekuniverse/MekanismUniverse.java
@@ -1,4 +1,4 @@
-package io.github.masyumero.examplemod;
+package io.github.masyumero.mekuniverse;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -6,10 +6,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(MekanismUniverse.MODID)
+public class MekanismUniverse {
 
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mekuniverse";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     @SuppressWarnings("removal")
